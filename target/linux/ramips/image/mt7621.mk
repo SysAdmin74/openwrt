b94a6139789601aab89d89dc34fa7079a0dc7751
SysAdmin74 <SysAdmin74@protonmail.com>
2024-02-26 00:21:31 +0800

    Add Support for ASUS 4G-AX56
    Original from pull 11021
    Original Author ccchen1984

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -419,6 +419,19 @@ define Device/asus_rt-ax54
 endef
 TARGET_DEVICES += asus_rt-ax54
 
+define Device/asus_4g-ax56
+  $(Device/nand)
+  DEVICE_VENDOR := ASUS
+  DEVICE_MODEL := 4G-AX56
+  IMAGE_SIZE := 51200k
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | \
+	check-size
+  DEVICE_PACKAGES := kmod-mt7915e kmod-usb3 kmod-usb-serial-option \
+	kmod-usb-net-cdc-ncm
+endef
+TARGET_DEVICES += asus_4g-ax56
+
 define Device/beeline_smartbox-flash
   $(Device/nand)
   $(Device/uimage-lzma-loader)
